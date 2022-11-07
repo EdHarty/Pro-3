9e718e973854379275e5d6b25005fb12681061ee
CarlGauss1 <eharty1@outlook.com>
2022-11-07 19:36:14 +0000

    Add different size ships

--- run.py
+++ run.py
@@ -1,3 +1,30 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+
+class Board:
+
+    """
+    Sets player board, game board and size of the board.
+    Sets ships sizes.
+    """
+
+    def __init__(self, player):
+
+        self.player = player
+
+        self.board = [[0 for bd in range(10)] for r in range(10)]
+
+        self.board_attacked = [[0 for bd in range(10)] for r in range(10)]
+
+        self.ships = []
+
+        self.append_ship(4)
+        self.append_ship(3)
+        self.append_ship(3)
+        self.append_ship(2)
+        self.append_ship(2)
+        self.append_ship(2)
+        self.append_ship(1)
+        self.append_ship(1)
+        self.append_ship(1)
+        self.append_ship(1)
